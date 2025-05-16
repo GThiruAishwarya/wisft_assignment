15a5e9b841c7237d1f8161a732203e2c6cdea6ad
GThiruAishwarya <167677392+GThiruAishwarya@users.noreply.github.com>
2025-05-16 10:20:26 +0530

    Create User.ts

---
+++ User.ts
@@ -0,0 +1,18 @@
+{
+  "name": "node_assignment",
+  "version": "1.0.0",
+  "description": "",
+  "scripts": {
+    "start": "ts-node src/server.ts"
+  },
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "typescript": "^5.0.2"
+  },
+  "dependencies": {
+    "mongodb": "^5.1.0",
+    "express": "^4.17.1",
+    "body-parser": "^1.19.0"
+  }
+}
